e6e79a8a9126f3afb676075acea248ace8360286
viethung9at1 <viethung.9at1@gmail.com>
2023-06-05 11:37:26 +0700

    Docker compose add

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Amazon.S3;
 using Saritasa;
+using Amazon.Runtime.CredentialManagement;
+
 var builder = WebApplication.CreateBuilder(args);
+var dbHost=Environment.GetEnvironmentVariable("DB_HOST");
+var dbName=Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword=Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString=$"Server={dbHost};Database={dbName};User Id=sa;Password={dbPassword};";
 // Add services to the container.
-builder.Services.AddDbContext<UserDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddDbContext<UserDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<UserDataContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
